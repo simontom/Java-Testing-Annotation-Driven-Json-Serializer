d9b49072d97ad21c0f658b90df012d9f5c015c23
Simon <simontom@volny.cz>
2018-03-31 00:21:09 +0200

    Testing getting type of element in array/list/map

--- src/simon/jsonserializer/Main.java
+++ src/simon/jsonserializer/Main.java
@@ -1,24 +1,47 @@
 package simon.jsonserializer;
 
 import org.json.JSONException;
-import org.json.JSONObject;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import simon.jsonserializer.dataobjects.Base;
 import simon.jsonserializer.jsonparser.exceptions.JsonParserException;
 
 public class Main {
+    public Base[] a;
+    public ArrayList<Base> al;
+    public HashMap<String, Base> hm;
+    public HashMap<String, Base> hm2;
 
-    public static void main(String[] args) throws JsonParserException, JSONException {
-        Character c = 'x';
-        JSONObject jo = new JSONObject();
-        jo.put("t", c);
+    public static void main(String[] args) throws JsonParserException, JSONException, NoSuchFieldException {
+        Field fieldA = Main.class.getDeclaredField("a");
+        Field fieldAL = Main.class.getDeclaredField("al");
+        Field fieldHM = Main.class.getDeclaredField("hm");
+        Field fieldHM2 = Main.class.getDeclaredField("hm2");
 
-        String joS = jo.toString();
-        JSONObject joD = new JSONObject(joS);
+        printInfo(fieldA);
+        printInfo(fieldAL);
+        printInfo(fieldHM);
+        printInfo(fieldHM2);
+    }
 
-        Object o1 = jo.get("t");
-        Object o2 = joD.get("t");
+    public static void printInfo(Field field) {
+        Type type = field.getGenericType();
+        System.out.println("type: " + type);
 
-        int karel = 1;
+        if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType) type;
+            System.out.println("raw type: " + pt.getRawType());
+            System.out.println("actual type args:");
+            for (Type t : pt.getActualTypeArguments()) {
+                System.out.println("    " + t);
+            }
+        }
+        System.out.print("\n\n");
     }
 
 }
