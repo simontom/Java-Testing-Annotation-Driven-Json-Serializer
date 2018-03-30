6c8f03e3bea2f9b9e7b0624956f04dee05cfe99f
Simon <simontom@volny.cz>
2018-03-31 01:35:17 +0200

    Testing getting type of element in array/list/map

--- src/simon/jsonserializer/Main.java
+++ src/simon/jsonserializer/Main.java
@@ -15,7 +15,7 @@ public class Main {
     public Base[] a;
     public ArrayList<Base> al;
     public HashMap<String, Base> hm;
-    public HashMap<String, Base> hm2;
+    public HashMap<Base, String> hm2;
 
     public static void main(String[] args) throws JsonParserException, JSONException, NoSuchFieldException {
         Field fieldA = Main.class.getDeclaredField("a");
@@ -23,22 +23,97 @@ public class Main {
         Field fieldHM = Main.class.getDeclaredField("hm");
         Field fieldHM2 = Main.class.getDeclaredField("hm2");
 
-        printInfo(fieldA);
-        printInfo(fieldAL);
-        printInfo(fieldHM);
-        printInfo(fieldHM2);
+//        printInfoArray(fieldA);
+//        printInfoArrayList(fieldAL);
+        printInfoHashMap(fieldHM);
+        printInfoHashMap(fieldHM2);
     }
 
-    public static void printInfo(Field field) {
+    private static void printInfoArray(Field field) {
+        Class<?> clazz = field.getType();
+        Class<?> genType = (Class) field.getGenericType(); // Use this one
+
+        System.out.println("genType: " + genType);
+        System.out.println("genType.isArray: " + genType.isArray());
+        System.out.println("genType.array_type: " + genType.getComponentType());
+
+        System.out.println("clazz: " + clazz);
+        System.out.println("clazz.isArray: " + clazz.isArray());
+        System.out.println("clazz.array_type: " + clazz.getComponentType());
+
+        System.out.print("\n\n");
+    }
+
+    private static void printInfoArrayList(Field field) {
+        Type genType = field.getGenericType();
+
+        System.out.println("genType is ParameterizedType: " + (genType instanceof ParameterizedType));
+
+        if (genType instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) genType;
+            System.out.println("raw genType: " + parameterizedType.getRawType());
+            System.out.println("actual genType args:");
+            for (Type t : parameterizedType.getActualTypeArguments()) {
+                Class c = (Class) t;
+                System.out.println("\t" + c);
+            }
+        }
+        System.out.print("\n\n");
+    }
+
+    private static void printInfoHashMap(Field field) {
+        Class<?> clazz = field.getType();
         Type type = field.getGenericType();
+        Type type2 = field.getType();
+
+        System.out.println("clazz: " + clazz);
         System.out.println("type: " + type);
+        System.out.println("type2: " + type2);
+
+        System.out.println("clazz.isArray: " + clazz.isArray());
+        System.out.println("type.isArray: " + type.getClass().isArray());
+        System.out.println("type2.isArray: " + type2.getClass().isArray());
+
+        System.out.println("type is ParameterizedType: " + (type instanceof ParameterizedType));
+        System.out.println("type2 is ParameterizedType: " + (type2 instanceof ParameterizedType));
 
         if (type instanceof ParameterizedType) {
             ParameterizedType pt = (ParameterizedType) type;
             System.out.println("raw type: " + pt.getRawType());
             System.out.println("actual type args:");
             for (Type t : pt.getActualTypeArguments()) {
+                Class c = (Class) t;
                 System.out.println("    " + t);
+                System.out.println("    " + c);
+            }
+        }
+        System.out.print("\n\n");
+    }
+
+    public static void printInfo(Field field) {
+        Class<?> clazz = field.getType();
+        Type type = field.getGenericType();
+        Type type2 = field.getType();
+
+        System.out.println("clazz: " + clazz);
+        System.out.println("type: " + type);
+        System.out.println("type2: " + type2);
+
+        System.out.println("clazz.isArray: " + clazz.isArray());
+        System.out.println("type.isArray: " + type.getClass().isArray());
+        System.out.println("type2.isArray: " + type2.getClass().isArray());
+
+        System.out.println("type is ParameterizedType: " + (type instanceof ParameterizedType));
+        System.out.println("type2 is ParameterizedType: " + (type2 instanceof ParameterizedType));
+
+        if (type instanceof ParameterizedType) {
+            ParameterizedType pt = (ParameterizedType) type;
+            System.out.println("raw type: " + pt.getRawType());
+            System.out.println("actual type args:");
+            for (Type t : pt.getActualTypeArguments()) {
+                Class c = (Class) t;
+                System.out.println("    " + t);
+                System.out.println("    " + t.getClass());
             }
         }
         System.out.print("\n\n");
