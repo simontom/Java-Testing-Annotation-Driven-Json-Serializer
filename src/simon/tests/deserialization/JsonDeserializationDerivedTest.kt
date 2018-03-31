d41ee75abf23445b750e009773deed2b3bd08f5e
Simon <simontom@volny.cz>
2018-03-31 14:57:48 +0200

    Added more tests

---
+++ src/simon/tests/deserialization/JsonDeserializationDerivedTest.kt
@@ -0,0 +1,22 @@
+package simon.tests.serialization
+
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runners.MethodSorters
+import simon.tests.JsonParserTestBase
+import simon.tests.TestData
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class JsonSerializationDerivedTest : JsonParserTestBase() {
+
+    @Test
+    fun a_serialize_DerivedClass() {
+        serializeAndCompare(TestData.derived_ok1_json, TestData.derived_ok1)
+    }
+
+    @Test
+    fun b_serialize_DerivedDoubledClass() {
+        serializeAndCompare(TestData.derivedDoubled_ok1_json, TestData.derivedDoubled_ok1)
+    }
+
+}
